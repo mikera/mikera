7c5dd9cf13a9ebd29375eb8611748e1040448286
mikera <Mike@Mike-VAIO>
2012-08-31 11:40:15 +0800

    Added MapEntry equals and hashcode

--- src/main/java/mikera/persistent/MapEntry.java
+++ src/main/java/mikera/persistent/MapEntry.java
@@ -3,6 +3,8 @@ package mikera.persistent;
 import java.io.Serializable;
 import java.util.Map;
 
+import mikera.util.Tools;
+
 /**
  * A persistent MapEntry
  * 
@@ -36,5 +38,22 @@ public final class MapEntry<K, V> implements Map.Entry<K,V>, Serializable {
 	@Override
 	public V setValue(V value) {
 		throw new UnsupportedOperationException();
+	}
+	
+	@SuppressWarnings("unchecked")
+	@Override
+	public boolean equals(Object o) {
+		if (this==o) return true;
+		if (!(o instanceof Map.Entry<?,?>)) return false;		
+		Map.Entry<K,V> ent = (Map.Entry<K,V>)o;
+		return (  Tools.equalsWithNulls(key, ent.getKey())
+				&&Tools.equalsWithNulls(value, ent.getValue()));
+	}
+	
+	public int hashCode() {
+		// defined same as java.util.Map.Entry
+		return (getKey()==null   ? 0 : getKey().hashCode()) ^
+			   (getValue()==null ? 0 : getValue().hashCode());
+		
 	}
 }
