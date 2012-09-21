ac864afab53cc9dc3ddbb6b96b0d4f231f24ba9e
mikera <Mike@Mike-VAIO>
2012-09-21 12:01:18 +0800

    Added checked exception for resources

--- src/test/java/mikera/util/TestResource.java
+++ src/test/java/mikera/util/TestResource.java
@@ -2,6 +2,8 @@ package mikera.util;
 
 import static org.junit.Assert.*;
 
+import java.io.FileNotFoundException;
+
 import mikera.util.Resource;
 
 import org.junit.Test;
@@ -10,7 +12,19 @@ public class TestResource {
 
 	@Test
 	public void testStringResource() {
-		String s = Resource.getResourceAsString("test-resource/test.txt");
+		String s;
+		try {
+			s = Resource.getResourceAsString("test-resource/test.txt");
+		} catch (FileNotFoundException e) {
+			throw new Error(e);
+		}
+	    assertEquals("hello",s);
+	}
+	
+	@Test (expected = FileNotFoundException.class)
+	public void testNotFoundResource() throws FileNotFoundException {
+		String s;
+		s = Resource.getResourceAsString("test-resource/non-existent-filename.txt");
 	    assertEquals("hello",s);
 	}
 
