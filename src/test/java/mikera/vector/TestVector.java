3c1b737ae4e440ca4b3cc364e55eb6f00a3616eb
mikera <Mike@Mike-VAIO>
2012-09-23 16:34:56 +0800

    Vector implementation improvements and tests

--- src/test/java/mikera/vector/TestVector.java
+++ src/test/java/mikera/vector/TestVector.java
@@ -1,6 +1,9 @@
 package mikera.vector;
 
 import static org.junit.Assert.*;
+
+import java.util.Arrays;
+
 import mikera.vector.Vector;
 
 import org.junit.Test;
@@ -15,7 +18,7 @@ public class TestVector {
 		assertEquals(10,v.get(10),0.0);
 		
 		Vector v2=v.subVector(5, 90);
-		assertEquals(90,v2.length);
+		assertEquals(90,v2.length());
 		assertEquals(15,v2.get(10),0.0);
 		
 		Vector v3=v2.subVector(5,80);
@@ -23,6 +26,19 @@ public class TestVector {
 		
 		v3.set(10, -99);
 		assertEquals(-99,v.get(20),0.0);
-
+	}
+	
+	@Test public void testClone() {
+		double[] data=new double[100];
+		for (int i=0; i<100; i++) data[i]=i;
+		Vector v=new Vector(data);	
+		
+		assertEquals(Arrays.hashCode(data),v.hashCode());
+		
+		Vector v2=new Vector(v);
+		assertEquals(v,v2);
+		
+		v.set(10,Math.PI);
+		assertTrue(!v.equals(v2));
 	}
 }
