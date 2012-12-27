ccf6b804b71b4b4ca6793d495e632b625f32de6a
mikera <mike.r.anderson.13@gmail.com>
2012-12-27 10:19:33 +0800

    Move staistics class out to mathz library

--- src/main/java/mikera/util/Statistics.java
+++
@@ -1,7 +0,0 @@
-package mikera.util;
-
-public class Statistics {
-	public double productOfNormalsStandardDeviation(double u1, double sd1, double u2, double sd2) {
-		return Math.sqrt((u1*u1)*(sd2*sd2)+(u2*u2)*(sd1*sd1)+(sd1*sd1)*(sd2*sd2));
-	}
-}
